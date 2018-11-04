856fb2e53d1e11dbe87d87be4154a67be219d4dc
Eric Gift <gift22@ccs.neu.edu>
2018-11-04 18:10:37 -0500

    initial commit

---
+++ src/main/java/Baze/HomeController.java
@@ -0,0 +1,13 @@
+package Baze;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
