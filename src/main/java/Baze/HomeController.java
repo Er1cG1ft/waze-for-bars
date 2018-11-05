4cb03d84f2a60795edeea14eb6343f7687e54260
Eric Gift <gift22@ccs.neu.edu>
2018-11-04 19:06:07 -0500

    adds home page view

--- src/main/java/Baze/HomeController.java
+++ src/main/java/Baze/HomeController.java
@@ -2,12 +2,16 @@ package Baze;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+	@GetMapping("/home")
+    public String home() {
+    	return "home"; 
     }
 }
