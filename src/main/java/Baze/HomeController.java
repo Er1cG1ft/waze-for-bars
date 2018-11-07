d653acac671432c513211b77b6fe31e72aaf9d8e
Eric Gift <gift22@ccs.neu.edu>
2018-11-07 14:51:49 -0500

    Loads first 20 bars from Google Places API & places pins, modals

--- src/main/java/Baze/HomeController.java
+++ src/main/java/Baze/HomeController.java
@@ -2,12 +2,41 @@ package Baze;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.PlaceType;
+import com.google.maps.model.PlacesSearchResponse;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
-    public String home() {
-    	return "home";
+	@RequestMapping(value = "home", method = RequestMethod.GET)
+    public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("home");
+		GeoApiContext context = new GeoApiContext.Builder()
+			    .apiKey("AIzaSyC9BQHyDExP8RYI5kBJmWq5ud0O2V5bsVc")
+			    .build();
+
+		LatLng coordinates = new LatLng(42.360081, -71.058884);
+		try {
+			PlacesSearchResponse bars = PlacesApi.nearbySearchQuery(context, coordinates)
+				.radius(5000)
+				.type(PlaceType.BAR)
+				.await();
+			String nextPageToken = bars.nextPageToken;
+			for (int i=0; i < bars.results.length; i++) {
+				System.out.println(bars.results[i].toString());
+			}
+			mav.addObject("bars", bars.results);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+    	return mav;
     }
 }
