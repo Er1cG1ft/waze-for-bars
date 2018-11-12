7d38b52a48df49e776871217d6188f175fb8fffa
Eric Gift <gift22@ccs.neu.edu>
2018-11-12 18:46:02 -0500

    adds user accounts and security

--- src/main/java/Baze/IndexController.java
+++ src/main/java/Baze/IndexController.java
@@ -1,12 +1,32 @@
 package Baze;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.PlaceType;
+import com.google.maps.model.PlacesSearchResponse;
  
 @Controller
 public class IndexController {
-    @RequestMapping("/")
-    String index(){
-        return "index";
+
+	@RequestMapping(value = "index", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("user", SecurityContextHolder.getContext()
+	            .getAuthentication()
+	            .getPrincipal());
+					
+	    return mav;
+	}
+	
+	@RequestMapping(value="/login")
+    String login(){
+        return "login";
     }
 }
